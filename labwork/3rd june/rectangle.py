54c75aff05f29ad27f856a609dd749f357e5ab64
pratyush0216 <pratyush0216@gmail.com>
2026-06-04 02:05:28 +0530

    Create rectangle.py

---
+++ labwork/3rd june/rectangle.py
@@ -0,0 +1,15 @@
+#program to calculate the area and parameter of rectangle
+l = int(input("enter the length of rectangle(in cm) : "))
+b = int(input("enter the breadth of rectangle(in cm) : "))
+#-----------------------------------------------------
+#calculation area 
+if l>0 and b>0:
+   area = l * b
+   print("the area of rectangle is : ", area, "sq.cm")
+#-------------------------------------------------------
+#Ccalculate perimeter
+   perimeter = 2 * (l + b)
+   print("the perimeter of rectangle is : ", perimeter, "cm")
+else:
+     print("invalid input. please enter positive values for length and breadth.")
+
