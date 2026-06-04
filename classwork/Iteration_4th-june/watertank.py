752f342057f77ddfe52716cb282b67b05e62d975
pratyush0216 <pratyush0216@gmail.com>
2026-06-04 11:58:23 +0530

    Create watertank.py

---
+++ classwork/Iteration_4th-june/watertank.py
@@ -0,0 +1,7 @@
+# program to shoe the water level in the tank
+water_level = 0
+while(water_level <=100):
+    print("water level " ,water_level,"liters")
+    water_level = water_level + 10
+#------------------------------------------------------------
+print("Tank is full")
