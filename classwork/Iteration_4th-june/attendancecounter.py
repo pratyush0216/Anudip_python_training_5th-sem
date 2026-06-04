395c75046fbe20b540daeb1f4c27a15478023f43
pratyush0216 <pratyush0216@gmail.com>
2026-06-04 12:05:59 +0530

    Create attendancecounter.py

---
+++ classwork/Iteration_4th-june/attendancecounter.py
@@ -0,0 +1,9 @@
+# WAP to count the attendance of students enter in the class
+student_count = 0 
+while(student_count < 30):
+    student_count += 1
+    print("student entered ")
+    print("Attendence count ", student_count)
+    
+#----------------------------------------------------------------
+print("Total students in the class are: ", student_count)
